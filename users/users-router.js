76965ca930067820a914aac2d8c8bb3d083d378a
Ruth Poliakon <r.poliakon@gmail.com>
2020-01-21 18:25:39 -0700

    users router and restricted middleware - mvp complete

--- users/users-router.js
+++ users/users-router.js
@@ -1,5 +1,17 @@
 const express = require("express");
+const restricted = require("../middleware/restricted");
+const usersModel = require("./users-model");
 
 const router = express.Router();
+
+router.get("/", restricted(), async (req, res, next) => {
+  try {
+    const users = await usersModel.get();
+
+    res.json(users);
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
